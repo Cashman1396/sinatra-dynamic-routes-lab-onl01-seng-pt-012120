2971d1660fc989476a9a5b42cd19cd678185f33c
Chris Ashman <chrisashman96@gmail.com>
2020-04-14 14:16:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,22 @@ class App < Sinatra::Base
     params[:name].reverse
   end
 
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end 
+
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do 
+      answer += params[:phrase]
+    end 
+
+    answer 
+  end 
+
+  
+  end
+
 
 end
